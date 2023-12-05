4628cfa4e4ee159c14f5273909bd78465a93338e
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-12-04 16:15:19 -0800

    refactor: import html files as strings

---
+++ src/forms/index.ts
@@ -0,0 +1,4 @@
+export { default as consentForm } from './consentform.html';
+export { default as consentFormPersonal } from './consentformCopy.html';
+export { default as survey } from './survey.html';
+export { default as thankYou } from './thankyoumessage.html';
