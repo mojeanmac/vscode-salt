9f86224b77c44dbdc9ed44610a162b3178c21e4d
Molly <mojeanmac@gmail.com>
2025-01-17 18:48:38 +0100

    dyn traitobjects + return values

--- crates/salt_ide/src/plugin/visit_hir.rs
+++ crates/salt_ide/src/plugin/visit_hir.rs
@@ -1,4 +1,4 @@
-use rustc_middle::ty::{Ty, TyCtxt, TyKind};
+use rustc_middle::ty::{Ty, TyCtxt, TyKind, ExistentialPredicate};
 use rustc_span::source_map::SourceMap;
 use rustc_span::def_id::DefId;
 use rustc_hir::intravisit::{self, Visitor};
@@ -14,8 +14,15 @@ use std::hash::{Hash, Hasher};
 struct Args {
     total: u32,
     mut_count: u32,
-    closure_count: u32,
+    closure_traits: Vec<String>,
     tys: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Return {
+    mutabl: bool,
+    closure_trait: Option<String>,
+    ty: String,
 }
 
 enum Block {
@@ -33,6 +40,7 @@ enum Block {
     },
     Def {
         args: Args,
+        ret: Return,
         recursive: bool,
         lines: usize,
         calls: u32,
@@ -55,6 +63,7 @@ enum BlockJson {
     },
     Def {
         args: serde_json::Value,
+        ret: serde_json::Value,
         recursive: bool,
         lines: usize,
         calls: u32,
@@ -76,8 +85,9 @@ impl Block {
             Block::LetExpr { id } => BlockJson::LetExpr {
                 id: id.to_string(),
             },
-            Block::Def { args, recursive, lines, calls } => BlockJson::Def {
+            Block::Def { args, ret, recursive, lines, calls } => BlockJson::Def {
                 args: serde_json::to_value(args).unwrap(),
+                ret: serde_json::to_value(ret).unwrap(),
                 recursive: *recursive,
                 lines: *lines,
                 calls: *calls,
@@ -142,8 +152,10 @@ impl<'tcx> Visitor<'tcx> for HirVisitor<'tcx> {
             match item.kind {
                 ItemKind::Fn(.., body_id) => { 
                     let args = visit_args(self.tcx, body_id);
+                    let ret = visit_return(self.tcx, body_id);
                     self.fns.insert(def_id, Block::Def {
                         args,
+                        ret,
                         recursive: false,
                         lines: line_count(self.source_map, item.span),
                         calls: 0,
@@ -174,8 +186,10 @@ impl<'tcx> Visitor<'tcx> for HirVisitor<'tcx> {
     fn visit_impl_item(&mut self, item: &'tcx rustc_hir::ImplItem<'tcx>) {
         if let rustc_hir::ImplItemKind::Fn(.., body_id) = item.kind {
             let args = visit_args(self.tcx, body_id);
+            let ret = visit_return(self.tcx, body_id);
             self.fns.insert(item.owner_id.to_def_id(), Block::Def {
                 args,
+                ret,
                 recursive: false,
                 lines: line_count(self.source_map, item.span),
                 calls: 0,
@@ -188,8 +202,10 @@ impl<'tcx> Visitor<'tcx> for HirVisitor<'tcx> {
         if let rustc_hir::TraitItemKind::Fn(..) = item.kind {
             let body_id = BodyId { hir_id: item.hir_id() };
             let args = visit_args(self.tcx, body_id);
+            let ret = visit_return(self.tcx, body_id);
             self.fns.insert(item.owner_id.to_def_id(), Block::Def {
                 args,
+                ret,
                 recursive: false,
                 lines: line_count(self.source_map, item.span),
                 calls: 0,
@@ -267,7 +283,7 @@ impl<'tcx> Visitor<'tcx> for HirVisitor<'tcx> {
 // count mutable arguments and closure arguments
 fn visit_args(tcx: TyCtxt, body_id: BodyId) -> Args {
     let mut mut_count = 0;
-    let mut closure_count = 0;
+    let mut closure_traits = Vec::new();
     let mut tys = Vec::new();
 
     let typeck_results = tcx.typeck(body_id.hir_id.owner);
@@ -276,9 +292,10 @@ fn visit_args(tcx: TyCtxt, body_id: BodyId) -> Args {
         let ty = typeck_results.node_type(param.hir_id);
         tys.push(ty.to_string());
 
-        if is_ty_closure(tcx, &ty, body_id.hir_id.owner.to_def_id()) {
-            closure_count += 1;
+        if let Some(closure_trait) = is_ty_closure(tcx, &ty, body_id.hir_id.owner.to_def_id()) {
+            closure_traits.push(closure_trait);
         }
+        
         match ty.kind() {
             TyKind::Adt(def, _) => {
                 if def.is_unsafe_cell() { mut_count += 1; }
@@ -299,40 +316,92 @@ fn visit_args(tcx: TyCtxt, body_id: BodyId) -> Args {
     Args {
         total: body.params.len() as u32,
         mut_count,
-        closure_count,
+        closure_traits,
         tys,
     }
 }
 
-fn is_ty_closure<'tcx>(tcx: TyCtxt<'tcx>, ty: &Ty<'tcx>, def_id: DefId) -> bool {
+fn visit_return(tcx: TyCtxt, body_id: BodyId) -> Return {
+    let body = tcx.hir().body(body_id);
+    let ty = tcx.typeck(body_id.hir_id.owner).node_type(body.value.hir_id);
+    let mutabl = match ty.kind() {
+        TyKind::Adt(def, _) => {
+            def.is_unsafe_cell()
+        },
+        TyKind::Ref(.., mut_ty) => {
+            mut_ty.is_mut()
+        },
+        TyKind::RawPtr(.., mut_ty) => {
+            mut_ty.is_mut()
+        },
+        _ => false,
+    };
+    let closure_trait = is_ty_closure(tcx, &ty, body_id.hir_id.owner.to_def_id());
+    Return {
+        mutabl,
+        closure_trait,
+        ty: ty.to_string(),
+    }
+}
+
+fn is_ty_closure<'tcx>(tcx: TyCtxt<'tcx>, ty: &Ty<'tcx>, def_id: DefId) -> Option<String> {
     let param_env = tcx.param_env(def_id);
     let lang_items = tcx.lang_items();
     
+    // check if closure impl
     if let Some(fn_trait) = lang_items.fn_trait() {
         if ty.does_implement_trait(tcx, param_env, fn_trait) {
-            return true;
+            return Some("Fn".to_string());
         }
     }
     if let Some(fn_trait) = lang_items.fn_mut_trait() {
         if ty.does_implement_trait(tcx, param_env, fn_trait) {
-            return true;
+            return Some("FnMut".to_string());
         }
     }
     if let Some(fn_trait) = lang_items.fn_once_trait() {
         if ty.does_implement_trait(tcx, param_env, fn_trait) {
-            return true;
+            return Some("FnOnce".to_string());
         }
     }
 
-    // match ty.kind() {
-    //     TyKind::TraitObject(..) => {
-    //         return true;
-    //     }
-    // }
+    if let Some(closure_trait) = is_dyn_closure(tcx, ty) {
+        return Some(closure_trait);
+    }
 
-    false
+    None
 }
 
+fn is_dyn_closure(tcx: TyCtxt, ty: &Ty) -> Option<String> {
+    match ty.kind() {
+        TyKind::Dynamic(preds, ..) => {
+            for pred in preds.iter() {
+                if let ExistentialPredicate::Trait(trait_pred) = pred.skip_binder() {
+                    if let Some(trait_name) = tcx.def_path_str(trait_pred.def_id).split("::").last() {
+                        match trait_name {
+                            "Fn" => return Some("Fn".to_string()),
+                            "FnMut" => return Some("FnMut".to_string()),
+                            "FnOnce" => return Some("FnOnce".to_string()),
+                            _ => {}
+                        }
+                    }
+                }
+            }
+            None
+        },
+        TyKind::Ref(_, inner_ty, _) => {
+            is_dyn_closure(tcx, inner_ty)
+        },
+        TyKind::Adt(..) => {
+            if let Some(inner_ty) = ty.boxed_ty() {
+                is_dyn_closure(tcx, &inner_ty)
+            } else {
+                None
+            }
+        },
+        _ => None,
+    }
+}
 
 fn ty_impls_iter<'tcx>(tcx: TyCtxt<'tcx>, ty: Ty<'tcx>, expr: &Expr<'tcx>) -> bool {
     if let Some(iterator_trait_def_id) = tcx.lang_items().iterator_trait() {
